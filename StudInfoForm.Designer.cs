61b2aec9e5f3b034bdfa7812a151c1d958cff5bb
heorhii.ivashchenko <heorhii.ivashchenko@nure.ua>
2018-07-12 16:31:25 +0300

    Добавьте файлы проекта.

---
+++ StudInfoForm.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace StudInfo
+{
+    partial class StudInfoForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLoadFromFiles = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.dataGridViewStudRates = new System.Windows.Forms.DataGridView();
+            this.ColumnSurname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnFName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnSName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnRate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnGroup = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnAddition = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.buttonGetFIOGroup = new System.Windows.Forms.Button();
+            this.buttonSaveToXML = new System.Windows.Forms.Button();
+            this.buttonDataGridClear = new System.Windows.Forms.Button();
+            this.buttonGetGroup = new System.Windows.Forms.Button();
+            this.textBoxFilter = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudRates)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonLoadFromFiles
+            // 
+            this.buttonLoadFromFiles.Location = new System.Drawing.Point(23, 46);
+            this.buttonLoadFromFiles.Name = "buttonLoadFromFiles";
+            this.buttonLoadFromFiles.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoadFromFiles.TabIndex = 0;
+            this.buttonLoadFromFiles.Text = "Загрузить";
+            this.buttonLoadFromFiles.UseVisualStyleBackColor = true;
+            this.buttonLoadFromFiles.Click += new System.EventHandler(this.buttonLoadFromFiles_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 75);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(55, 212);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // dataGridViewStudRates
+            // 
+            this.dataGridViewStudRates.AllowUserToAddRows = false;
+            this.dataGridViewStudRates.AllowUserToDeleteRows = false;
+            this.dataGridViewStudRates.AllowUserToResizeRows = false;
+            this.dataGridViewStudRates.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridViewStudRates.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewStudRates.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnSurname,
+            this.ColumnFName,
+            this.ColumnSName,
+            this.ColumnRate,
+            this.ColumnGroup,
+            this.ColumnAddition});
+            this.dataGridViewStudRates.Location = new System.Drawing.Point(73, 76);
+            this.dataGridViewStudRates.MultiSelect = false;
+            this.dataGridViewStudRates.Name = "dataGridViewStudRates";
+            this.dataGridViewStudRates.ReadOnly = true;
+            this.dataGridViewStudRates.RowHeadersVisible = false;
+            this.dataGridViewStudRates.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewStudRates.ShowCellErrors = false;
+            this.dataGridViewStudRates.ShowCellToolTips = false;
+            this.dataGridViewStudRates.ShowEditingIcon = false;
+            this.dataGridViewStudRates.ShowRowErrors = false;
+            this.dataGridViewStudRates.Size = new System.Drawing.Size(648, 215);
+            this.dataGridViewStudRates.TabIndex = 2;
+            // 
+            // ColumnSurname
+            // 
+            this.ColumnSurname.Frozen = true;
+            this.ColumnSurname.HeaderText = "Surname";
+            this.ColumnSurname.Name = "ColumnSurname";
+            this.ColumnSurname.ReadOnly = true;
+            // 
+            // ColumnFName
+            // 
+            this.ColumnFName.Frozen = true;
+            this.ColumnFName.HeaderText = "FirstName";
+            this.ColumnFName.Name = "ColumnFName";
+            this.ColumnFName.ReadOnly = true;
+            // 
+            // ColumnSName
+            // 
+            this.ColumnSName.Frozen = true;
+            this.ColumnSName.HeaderText = "SecondName";
+            this.ColumnSName.Name = "ColumnSName";
+            this.ColumnSName.ReadOnly = true;
+            // 
+            // ColumnRate
+            // 
+            this.ColumnRate.Frozen = true;
+            this.ColumnRate.HeaderText = "Rate";
+            this.ColumnRate.Name = "ColumnRate";
+            this.ColumnRate.ReadOnly = true;
+            // 
+            // ColumnGroup
+            // 
+            this.ColumnGroup.Frozen = true;
+            this.ColumnGroup.HeaderText = "Group";
+            this.ColumnGroup.Name = "ColumnGroup";
+            this.ColumnGroup.ReadOnly = true;
+            // 
+            // ColumnAddition
+            // 
+            this.ColumnAddition.Frozen = true;
+            this.ColumnAddition.HeaderText = "Addition";
+            this.ColumnAddition.Name = "ColumnAddition";
+            this.ColumnAddition.ReadOnly = true;
+            // 
+            // buttonGetFIOGroup
+            // 
+            this.buttonGetFIOGroup.Location = new System.Drawing.Point(147, 17);
+            this.buttonGetFIOGroup.Name = "buttonGetFIOGroup";
+            this.buttonGetFIOGroup.Size = new System.Drawing.Size(146, 23);
+            this.buttonGetFIOGroup.TabIndex = 3;
+            this.buttonGetFIOGroup.Text = "Фамилия И.О. (Группа)";
+            this.buttonGetFIOGroup.UseVisualStyleBackColor = true;
+            this.buttonGetFIOGroup.Click += new System.EventHandler(this.buttonGetFIOGroup_Click);
+            // 
+            // buttonSaveToXML
+            // 
+            this.buttonSaveToXML.Location = new System.Drawing.Point(23, 17);
+            this.buttonSaveToXML.Name = "buttonSaveToXML";
+            this.buttonSaveToXML.Size = new System.Drawing.Size(75, 23);
+            this.buttonSaveToXML.TabIndex = 4;
+            this.buttonSaveToXML.Text = "SaveToXML";
+            this.buttonSaveToXML.UseVisualStyleBackColor = true;
+            this.buttonSaveToXML.Click += new System.EventHandler(this.buttonSaveToXML_Click);
+            // 
+            // buttonDataGridClear
+            // 
+            this.buttonDataGridClear.Location = new System.Drawing.Point(337, 13);
+            this.buttonDataGridClear.Name = "buttonDataGridClear";
+            this.buttonDataGridClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonDataGridClear.TabIndex = 5;
+            this.buttonDataGridClear.Text = "Clear";
+            this.buttonDataGridClear.UseVisualStyleBackColor = true;
+            this.buttonDataGridClear.Click += new System.EventHandler(this.buttonDataGridClear_Click);
+            // 
+            // buttonGetGroup
+            // 
+            this.buttonGetGroup.Location = new System.Drawing.Point(147, 46);
+            this.buttonGetGroup.Name = "buttonGetGroup";
+            this.buttonGetGroup.Size = new System.Drawing.Size(146, 23);
+            this.buttonGetGroup.TabIndex = 3;
+            this.buttonGetGroup.Text = " (Группа)";
+            this.buttonGetGroup.UseVisualStyleBackColor = true;
+            this.buttonGetGroup.Click += new System.EventHandler(this.buttonGetGroup_Click);
+            // 
+            // textBoxFilter
+            // 
+            this.textBoxFilter.Location = new System.Drawing.Point(337, 46);
+            this.textBoxFilter.Name = "textBoxFilter";
+            this.textBoxFilter.Size = new System.Drawing.Size(100, 20);
+            this.textBoxFilter.TabIndex = 6;
+            this.textBoxFilter.TextChanged += new System.EventHandler(this.textBoxFilter_TextChanged);
+            // 
+            // StudRatingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(733, 303);
+            this.Controls.Add(this.textBoxFilter);
+            this.Controls.Add(this.buttonDataGridClear);
+            this.Controls.Add(this.buttonSaveToXML);
+            this.Controls.Add(this.buttonGetGroup);
+            this.Controls.Add(this.buttonGetFIOGroup);
+            this.Controls.Add(this.dataGridViewStudRates);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.buttonLoadFromFiles);
+            this.Name = "StudRatingForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.StudRatingForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudRates)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLoadFromFiles;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DataGridView dataGridViewStudRates;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnSurname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnFName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnSName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnRate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnGroup;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnAddition;
+        private System.Windows.Forms.Button buttonGetFIOGroup;
+        private System.Windows.Forms.Button buttonSaveToXML;
+        private System.Windows.Forms.Button buttonDataGridClear;
+        private System.Windows.Forms.Button buttonGetGroup;
+        private System.Windows.Forms.TextBox textBoxFilter;
+    }
+}
+
---
+++ StudInfoForm.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using org.apache.pdfbox.pdmodel;
+using org.apache.pdfbox.util;
+
+namespace StudInfo
+{
+    public struct Student
+    {
+        public string Surname;
+        public string FName;
+        public string SName;
+        public string Rate;
+        public string Group;
+        public string Addition;
+
+        public Student(string surname, string fname, string sname, string rate, string group, string addition)
+        {
+            FName = fname;
+            Surname = surname;
+            SName = sname;
+            Rate = rate;
+            Group = group;
+            Addition = addition;
+        }
+
+
+        public override string ToString()
+        {
+            return Surname + " " + FName + " " + SName + " " + Rate + " " + Group + " " + Addition;
+        }
+    }
+
+    public partial class StudInfoForm : Form
+    {
+        //string path = "test.pdf";
+        List<Student> StudRates = new List<Student>();
+        public StudInfoForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonLoadFromFiles_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog
+            {
+                Multiselect = true
+            };
+            DialogResult dialogResult  = ofd.ShowDialog();
+            if(dialogResult== DialogResult.Cancel || dialogResult== DialogResult.Abort || dialogResult== DialogResult.No)
+            {
+                return;
+            }
+
+            foreach(string path in ofd.FileNames)
+            {
+                StudRates.AddRange(ReadFromPDF(path));
+            }
+            FillStudRatesGrid();            
+        }
+
+        private void FillStudRatesGrid()
+        {
+            foreach (Student stud in StudRates)
+            {
+                DataGridViewTextBoxCell cellSurname = new DataGridViewTextBoxCell();
+                cellSurname.Value = stud.Surname;
+                DataGridViewTextBoxCell cellFName = new DataGridViewTextBoxCell();
+                cellFName.Value = stud.FName;
+                DataGridViewTextBoxCell cellSName = new DataGridViewTextBoxCell();
+                cellSName.Value = stud.SName;
+                DataGridViewTextBoxCell cellRate = new DataGridViewTextBoxCell();
+                cellRate.Value = stud.Rate;
+                DataGridViewTextBoxCell cellGroup = new DataGridViewTextBoxCell();
+                cellGroup.Value = stud.Group;
+                DataGridViewTextBoxCell cellAddition = new DataGridViewTextBoxCell();
+                cellAddition.Value = stud.Addition;
+
+                DataGridViewRow row = new DataGridViewRow();
+                row.Cells.Add(cellSurname);
+                row.Cells.Add(cellFName);
+                row.Cells.Add(cellSName);
+                row.Cells.Add(cellRate);
+                row.Cells.Add(cellGroup);
+                row.Cells.Add(cellAddition);
+                dataGridViewStudRates.Rows.Add(row);
+                //listBox1.Items.Add(stud.ToString());
+            }
+        }
+
+        private IEnumerable<Student> ReadFromPDF(string path)
+        {
+            List<Student> students = new List<Student>();
+            string text = "";
+
+            PDDocument document = PDDocument.load(path);
+            PDFTextStripper stripper = new PDFTextStripper();
+            text = stripper.getText(document);
+
+            List<string> contentFirst = new List<string>();
+            string[] separated = text.Split(null);
+            foreach (string str in separated)
+            {
+                if (str != "")
+                {
+                    contentFirst.Add(str);
+                }
+            }
+            int indexPIB = contentFirst.IndexOf("П.І.Б");
+            contentFirst.RemoveRange(0, indexPIB);
+            for (int i = 5; i < contentFirst.Count; i++)
+            {
+                if (Char.IsLower(contentFirst[i][0]))
+                {
+                    i++;
+                    continue;
+                }
+                Student stud = new Student();
+                stud.Surname = contentFirst[i];
+                i++;
+                stud.FName = contentFirst[i];
+                i++;
+                stud.SName = contentFirst[i];
+                i++;
+                // если имен четыре
+                if (Char.IsUpper(contentFirst[i][0]) || contentFirst[i]=="-") // костыль для обработки коряво записаного Буй Ван Тунга -
+                {
+                    stud.SName += " " + contentFirst[i];
+                    i++;
+                }
+
+                if (contentFirst[i] == "не")
+                {
+                    if (Char.IsUpper(contentFirst[i + 1][0])) // ячейка разделена разрывом страницы
+                    {
+                        stud.Rate = contentFirst[i] + " " + contentFirst[i+3];
+                        contentFirst.RemoveAt(i + 3);
+                    }
+                    else
+                    {
+                        stud.Rate = contentFirst[i] + " " + contentFirst[++i];
+                    }
+                    i++;
+                }
+                else
+                {
+                    stud.Rate = contentFirst[i];
+                    i++;
+                }
+                stud.Group = contentFirst[i];
+                i++;
+                while (Char.IsLower(contentFirst[i][0]))
+                {
+                    stud.Addition += contentFirst[i] + " ";
+                    i++;
+                    if (i == contentFirst.Count)
+                    {
+                        break;
+                    }
+                }
+                if (stud.Addition != null)
+                    stud.Addition.Trim();
+                i--;
+                students.Add(stud);
+            }
+            return students;
+        }
+
+        private void buttonGetFIOGroup_Click(object sender, EventArgs e)
+        {
+            string selected = "";
+            if (dataGridViewStudRates.SelectedRows != null)
+            {
+                selected = dataGridViewStudRates.SelectedRows[0].Cells[0].Value.ToString()
+                    + " " + dataGridViewStudRates.SelectedRows[0].Cells[1].Value.ToString()[0]
+                    + "." + dataGridViewStudRates.SelectedRows[0].Cells[2].Value.ToString()[0]
+                    + ". ("+ dataGridViewStudRates.SelectedRows[0].Cells[4].Value.ToString() + ")";
+                //MessageBox.Show(selected);
+                Clipboard.SetData(DataFormats.Text, (Object)selected);
+            }
+        }
+
+        private void StudRatingForm_Load(object sender, EventArgs e)
+        {
+            string[] xmlFiles = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.xml");
+            for(int i=0;i< xmlFiles.Length;i++)
+            {
+                xmlFiles[i] = Path.GetFileName(xmlFiles[i]);
+            }
+            listBox1.Items.AddRange(xmlFiles);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dataGridViewStudRates.Rows.Clear();
+            ReadFromXML(listBox1.SelectedItem.ToString());
+            FillStudRatesGrid();
+        }
+
+        private void ReadFromXML(string path)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Student>));
+            FileStream fs = new FileStream(path, FileMode.Open);
+            XmlReader reader = XmlReader.Create(fs);
+            StudRates = (List<Student>)serializer.Deserialize(reader);
+            fs.Close();
+        }
+
+        private void buttonSaveToXML_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            DialogResult dialogResult = sfd.ShowDialog();
+            if (dialogResult == DialogResult.Cancel || dialogResult == DialogResult.Abort || dialogResult == DialogResult.No)
+            {
+                return;
+            }
+
+            SerializeObject(sfd.FileName);
+        }
+
+        private void SerializeObject(string filename)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Student>));
+            Stream fs = new FileStream(filename, FileMode.Create);
+            XmlWriter writer = new XmlTextWriter(fs, Encoding.Unicode);
+            serializer.Serialize(writer, StudRates);
+            writer.Close();
+        }
+
+        private void buttonDataGridClear_Click(object sender, EventArgs e)
+        {
+            StudRates.Clear();
+            dataGridViewStudRates.Rows.Clear();
+        }
+
+        private void buttonGetGroup_Click(object sender, EventArgs e)
+        {
+            string selected = "";
+            if (dataGridViewStudRates.SelectedRows != null)
+            {
+                selected = " (" + dataGridViewStudRates.SelectedRows[0].Cells[4].Value.ToString() + ")";
+                //MessageBox.Show(selected);
+                Clipboard.SetData(DataFormats.Text, (Object)selected);
+            }
+        }
+
+        private void textBoxFilter_TextChanged(object sender, EventArgs e)
+        {
+            for (int u = 0; u < dataGridViewStudRates.RowCount; u++)
+            {
+                if (dataGridViewStudRates.Rows[u].Cells[0].Value.ToString().StartsWith(textBoxFilter.Text))
+                {
+                    dataGridViewStudRates.Rows[u].Visible = true;
+                }
+                else
+                {
+                    dataGridViewStudRates.Rows[u].Visible = false;
+                }
+            }
+        }
+    }
+}
